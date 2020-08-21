0c94d56971a5ac999c18fac630d217c6e894480f
Alejandro <a.alcaraz@uniandes.edu.co>
2020-08-20 21:25:14 -0500

    Cree metodo constructor de main y controlador

--- src/Controller/controlador.java
+++ src/Controller/controlador.java
@@ -1,5 +1,53 @@
 package Controller;
 
-public class controlador {
+import java.util.Scanner;
 
+import model.logic.Modelo;
+import view.View;
+
+public class controlador {
+	
+	/* Instancia del Modelo*/
+	private Modelo modelo;
+	
+	/* Instancia de la Vista*/
+	private View view;
+
+	public controlador()
+	{
+		view = new View();
+		modelo = new Modelo();
+	}
+	
+	public void run()
+	{
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,12 @@
 package main;
 
+
+import controller.Controller;
+
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	public static void main(String[] args) 
+	{
+		controlador controller = new controlador();
+		controller.run();
+	}	
 }
